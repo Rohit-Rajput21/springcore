cb37081f3fe7abbd4e08f823efb0cbf099830394
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-01-16 19:31:08 +0530

    Initial Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext applicationContext= new ClassPathXmlApplicationContext("config.xml");
+       Student student1 =(Student)applicationContext.getBean("student1");
+       Student student2 =(Student)applicationContext.getBean("student2");
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
