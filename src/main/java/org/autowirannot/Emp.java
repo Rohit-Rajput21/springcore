3727403db48fcc45c87af9915ec08fd711042620
Rohit-Rajput21 <rohit232425rr@gmail.com>
2023-01-26 12:01:49 +0530

    commit: Qualifier Annotaion for ambiguity solution

--- src/main/java/org/autowirannot/Emp.java
+++ src/main/java/org/autowirannot/Emp.java
@@ -1,11 +1,13 @@
 package org.autowirannot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Emp {
-   //@Autowired   --  autowiring using property
+  // @Autowired  // --  autowiring using property
+  // @Qualifier("address1")
     private Address address;
-    @Autowired  //-- using constructor
+   // @Autowired  //-- using constructor
     public Emp(Address address) {
         System.out.println("Emp class parameterized constructor");
         this.address = address;
@@ -20,7 +22,8 @@ public class Emp {
         return address;
     }
 
-   // @Autowired   -- autowiring for setter method
+    @Autowired   //-- autowiring for setter method
+    @Qualifier("address1")
     public void setAddress(Address address) {
         System.out.println("Employee class setter method");
         this.address = address;
